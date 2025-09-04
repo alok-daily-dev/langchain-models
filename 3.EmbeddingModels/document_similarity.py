829bef95bc76dee787746a0860834618a277b3fb
alok-daily-dev <alokdailydev@gmail.com>
2025-09-04 08:24:54 +0000

    LangChain Models Practice has been completed

--- 3.EmbeddingModels/document_similarity.py
+++ 3.EmbeddingModels/document_similarity.py
@@ -4,18 +4,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 load_dotenv()
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", dimemsions=300)
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", dimensions=300)
 
 documents = [
     "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
-    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "MS Dhoni is a former Indian cricket team captain famous for his calm demeanor and finishing skills.",
     "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
     "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
     "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
 ]
 
-query = "Who is the captain of the Indian cricket team?"
+query = "Who is former captain of the Indian cricket team?"
 doc_embeddings = embeddings.embed_documents(documents)
 query_embedding = embeddings.embed_query(query)
 
-print(cosine_similarity([query_embedding], doc_embeddings))
+similarity_score = cosine_similarity([query_embedding], doc_embeddings)[0]
+
+best_match_index = np.argmax(similarity_score)
+
+print(f"Best matching document: {documents[best_match_index]}")
