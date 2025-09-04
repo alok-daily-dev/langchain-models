c7730d1164c5f677efa3af053388141d9e87cd07
alok-daily-dev <alokdailydev@gmail.com>
2025-09-04 07:17:08 +0000

    Hugging Face Embedding Updated

--- 3.EmbeddingModels/hf_embedding.py
+++ 3.EmbeddingModels/hf_embedding.py
@@ -5,7 +5,8 @@ embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L
 documents = [
     "Delhi is the capital of India",
     "Kolkata is the capital of West Bengal",
-    "Paris is the capital of France"
+    "Paris is the capital of France",
+    "Berlin is the capital of Germany",
 ]
 
 vector = embedding.embed_documents(documents)
