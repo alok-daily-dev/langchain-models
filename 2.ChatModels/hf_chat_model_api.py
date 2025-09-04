4c2653aecdf409ee9bb1265cac9a75c31a140b71
alok-daily-dev <alokdailydev@gmail.com>
2025-09-04 03:21:16 +0000

    Hugging Face

---
+++ 2.ChatModels/hf_chat_model_api.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="chat-completion"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of Pakistan? Just give me the name")
+
+print(result.content)
